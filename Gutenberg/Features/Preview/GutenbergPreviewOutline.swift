e8563226b50c22a98b429d7ffe64ff0be7f1301d
Marquis Kurt <software@marquiskurt.net>
2022-12-07 12:58:02 -0500

    :bug: Resolve GB-4
    Some events in the preview outline were left blank because the who
    contents were missing. This is common for questions and refresh
    events.
    
    This commit adds a more robust solution to show various event
    types.
    
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- Gutenberg/Features/Preview/GutenbergPreviewOutline.swift
+++ Gutenberg/Features/Preview/GutenbergPreviewOutline.swift
@@ -1,8 +1,12 @@
 //
 //  GutenbergPreviewOutline.swift
-//  Gutenberg
-//
 //  Created by Marquis Kurt on 12/6/22.
+//  This file is part of Gutenberg.
+//
+//  Gutenberg is non-violent software: you can use, redistribute, and/or modify it under the terms of the CNPLv7+ as
+//  found in the LICENSE file in the source code root directory or at <https://git.pixie.town/thufie/npl-builder>.
+//
+//  Gutenberg comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law. See the CNPL for details.
 //
 
 import SwiftUI
@@ -18,8 +22,22 @@ struct GutenbergPreviewOutline: View {
                 ForEach(Array(timeline.enumerated()), id: \.element.id) { (index, event) in
                     Label {
                         VStack(alignment: .leading) {
-                            Text(event.what)
-                                .font(.subheadline)
+                            Group {
+                                switch event.type {
+                                case .refresh:
+                                    Label(
+                                        "Refresh \(event.refresh?.count ?? 0) triggers",
+                                        systemImage: "arrow.clockwise.circle"
+                                    )
+                                case .question:
+                                    Label(event.question?.question ?? "Question", systemImage: "questionmark.circle")
+                                case .dialogue, .comment:
+                                    Label(event.what, systemImage: "text.bubble")
+                                default:
+                                    Text("Unknown event")
+                                }
+                            }
+                            .font(.subheadline)
                         }
                     } icon: {
                         Text("\(index + 1)")
